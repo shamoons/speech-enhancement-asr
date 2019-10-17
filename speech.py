f29f938a8e43b65e67c98e97623938a3dc545c34
Shamoon Siddiqui <shamoon@develop.io>
2019-10-16 22:41:49 -0400

    Able to read a file

--- speech.py
+++ speech.py
@@ -3,25 +3,27 @@ from os import environ, path
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
 
-MODELDIR = "pocketsphinx/model"
-DATADIR = "pocketsphinx/test/data"
+MODELDIR = "/home/shamoon/.local/share/virtualenvs/speech-enhancement-asr-a7TdkoLa/lib/python3.6/site-packages/pocketsphinx/model"
+DATADIR = "/home/shamoon/.local/share/virtualenvs/speech-enhancement-asr-a7TdkoLa/lib/python3.6/site-packages/pocketsphinx/data"
 
 # Create a decoder with certain model
 config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+config.set_string('-hmm', path.join(MODELDIR, 'en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'cmudict-en-us.dict'))
 decoder = Decoder(config)
 
 # Decode streaming data.
 decoder = Decoder(config)
 decoder.start_utt()
-stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
+stream = open('84-121123-0000.wav', 'rb')
+# stream = open('data/LibriSpeech/dev-clean/84/121123/84-121123-0001.flac', 'rb')
+# stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
 while True:
-  buf = stream.read(1024)
-  if buf:
-    decoder.process_raw(buf, False, False)
-  else:
-    break
+    buf = stream.read(1024)
+    if buf:
+        decoder.process_raw(buf, True, False)
+    else:
+        break
 decoder.end_utt()
-print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+print('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
