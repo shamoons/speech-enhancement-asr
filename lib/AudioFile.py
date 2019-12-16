4f584584afe6bb876d2492e3f0f269a302d18cb2
Shamoon Siddiqui <shamoons@users.noreply.github.com>
2019-12-16 07:37:01 -0800

    Enhancement/segan (#11)
    * Segan working
    
    * Segan enhancement

--- lib/AudioFile.py
+++ lib/AudioFile.py
@@ -41,11 +41,16 @@ class AudioFile:
 
     def load_random(self, subset='test-clean'):
         book_ids = os.listdir(os.path.join(self.DATA_PATH, subset))
+        book_ids.remove('.DS_Store')
+
         book_id = pydash.sample(book_ids)
         clean_path = os.path.join(self.DATA_PATH, subset, book_id)
 
-        chapter_id = pydash.sample(os.listdir(clean_path))
+        chapter_ids = os.listdir(clean_path)
+        chapter_ids.remove('.DS_Store')
+        chapter_id = pydash.sample(chapter_ids)
         chapter_path = os.path.join(clean_path, chapter_id)
+
         transcripts_list = [s for s in os.listdir(chapter_path) if s.endswith('.flac')]
         chosen_transcript = pydash.sample(transcripts_list)
 
--- lib/SpeechEnhance.py
+++ lib/SpeechEnhance.py
@@ -1,13 +1,42 @@
 from scipy.signal import wiener
+import json
+import torch
+import soundfile as sf
 from .segan_pytorch.segan.models import SEGAN
+from .segan_pytorch.segan.datasets import normalize_wave_minmax
+from .segan_pytorch.segan.datasets import pre_emphasize
+
+class ArgParser(object):
+
+    def __init__(self, args):
+        for k, v in args.items():
+            setattr(self, k, v)
 
 class SpeechEnhance:
     def __init__(self):
-        self.segan = SEGAN()
+        with open("data/models/segan_v1.1/train.opts", 'r') as cfg_f:
+            self.segan_args = ArgParser(json.load(cfg_f))
+        # {
+        #     "g_pretrained_ckpt": "data/models/segan_v1.1/segan+_generator.ckpt",
+        #     "seed": 111,
+        #     "synthesis_path": "segan_enhanced"
+
+        # }
+
+
+        self.segan = SEGAN(self.segan_args)
+        self.segan.G.load_pretrained("data/models/segan_v1.1/segan+_generator.ckpt", True)
+        self.segan.G.eval()
 
     def wiener(self, audio_signal):
         return wiener(audio_signal)
 
     def segan_enhance(self, audio_signal):
-        print('hi')
-        return
+        wav = normalize_wave_minmax(audio_signal)
+        wav = pre_emphasize(wav)
+        pwav = torch.FloatTensor(wav).view(1,1,-1)
+
+        g_wav, g_c = self.segan.generate(pwav)
+        sf.write('file.wav', g_wav, 16000)
+
+        return g_wav
