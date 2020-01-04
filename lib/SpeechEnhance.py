eb4a29f6f6ad9a6ed7a4f251f01ac634415879fd
Shamoon Siddiqui <shamoon@develop.io>
2020-01-04 15:27:05 -0800

    DeepXi

--- lib/SpeechEnhance.py
+++ lib/SpeechEnhance.py
@@ -7,7 +7,7 @@ from scipy.signal import wiener
 from .segan_pytorch.segan.models import SEGAN
 from .segan_pytorch.segan.datasets import normalize_wave_minmax
 from .segan_pytorch.segan.datasets import pre_emphasize
-from .DeepXiNet import DeepXiNet
+# from .DeepXiNet import DeepXiNet
 
 class ArgParser(object):
 
@@ -24,13 +24,20 @@ class SpeechEnhance:
         self.segan.G.load_pretrained("data/models/segan_v1.1/segan+_generator.ckpt", True)
         self.segan.G.eval()
 
-        self.deepxi = DeepXiNet()
+        # self.deepxi = DeepXiNet()
     def convert_to_int(self, audio_signal):
         enhanced_signal = (audio_signal * 32767).astype(np.int16)
         return enhanced_signal
 
     def wiener(self, audio_signal):
-        return self.convert_to_int(wiener(audio_signal))
+        enhanced_signal = wiener(audio_signal)
+        print(audio_signal)
+        print(np.min(audio_signal),np.max(audio_signal))
+
+        print(enhanced_signal)
+        print(np.min(enhanced_signal),np.max(enhanced_signal))
+        return enhanced_signal
+        # return self.convert_to_int(wiener(audio_signal))
 
     def segan_enhance(self, audio_signal):
         wav = normalize_wave_minmax(audio_signal)
@@ -41,7 +48,7 @@ class SpeechEnhance:
 
         return self.convert_to_int(g_wav)
     
-    def deepxi_enhance(self, audio_signal):
-        with tf.Session() as sess:
-            self.deepxi.saver.restore(sess, 'lib/deepxi/model/3e/epoch-173')
-            return
+    # def deepxi_enhance(self, audio_signal):
+    #     with tf.Session() as sess:
+    #         self.deepxi.saver.restore(sess, 'lib/deepxi/model/3e/epoch-173')
+    #         return
