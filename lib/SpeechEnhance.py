f934cccfa5dfe94ec5b05bcbc773618832a46afb
Shamoon Siddiqui <shamoon@develop.io>
2020-01-04 16:11:17 -0800

    Fixed wiener

--- lib/SpeechEnhance.py
+++ lib/SpeechEnhance.py
@@ -9,11 +9,13 @@ from .segan_pytorch.segan.datasets import normalize_wave_minmax
 from .segan_pytorch.segan.datasets import pre_emphasize
 # from .DeepXiNet import DeepXiNet
 
+
 class ArgParser(object):
 
     def __init__(self, args):
         for k, v in args.items():
             setattr(self, k, v)
+
 
 class SpeechEnhance:
     def __init__(self):
@@ -21,7 +23,8 @@ class SpeechEnhance:
             self.segan_args = ArgParser(json.load(cfg_f))
 
         self.segan = SEGAN(self.segan_args)
-        self.segan.G.load_pretrained("data/models/segan_v1.1/segan+_generator.ckpt", True)
+        self.segan.G.load_pretrained(
+            "data/models/segan_v1.1/segan+_generator.ckpt", True)
         self.segan.G.eval()
 
         # self.deepxi = DeepXiNet()
@@ -30,17 +33,12 @@ class SpeechEnhance:
         return enhanced_signal
 
     def wiener(self, audio_signal):
-        enhanced_signal = wiener(audio_signal)
-        print('audio_signal')
-        print(audio_signal)
-        print(np.min(audio_signal),np.max(audio_signal))
+        float_audio_signal = (audio_signal / 32767).astype(np.float64)
+        enhanced_signal = wiener(float_audio_signal)
 
-        print('enhanced_signal')
-        # enhanced_signal = (enhanced_signal).astype(np.int16)
-        print(enhanced_signal)
-        print(np.min(enhanced_signal),np.max(enhanced_signal))
+        enhanced_signal = self.convert_to_int(enhanced_signal)
+
         return enhanced_signal
-        # return self.convert_to_int(wiener(audio_signal))
 
     def segan_enhance(self, audio_signal):
         wav = normalize_wave_minmax(audio_signal)
@@ -50,7 +48,7 @@ class SpeechEnhance:
         g_wav, g_c = self.segan.generate(pwav)
 
         return self.convert_to_int(g_wav)
-    
+
     # def deepxi_enhance(self, audio_signal):
     #     with tf.Session() as sess:
     #         self.deepxi.saver.restore(sess, 'lib/deepxi/model/3e/epoch-173')
