ac63533c6eb470c40d590314697fcb2f80a5d9b2
Shamoon Siddiqui <shamoon@develop.io>
2020-01-20 11:07:28 -0500

    Fixed shell

--- evaluate.sh
+++ evaluate.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-echo "Noise $1"
-echo "Enhancement $2"
+printf 'Noise %s\n' "$1"
+printf 'Enhancement %s\n' "$2"
 
-for snr in 0 5 10 15 20 25
-do
-    if [ -z "$2" ]; then
-        set -- --noise "$1" --snr $snr --iterations 1250
-    else
-        set -- --noise "$1" --snr $snr --iterations 1250 --enhancement "$2"
-    fi
-    pipenv run python evaluate.py "$@" >> $1.$snr.$2.out
+for snr in 0 5 10 15 20 25; do
+    pipenv run python evaluate.py \
+        --noise "$1" \
+        --snr "$snr" \
+        --iterations 2 \
+        ${2:+--enhancement "$2"}
 done
