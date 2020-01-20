cd99fa098d32f63cade63ff796cdaa779f07053b
Shamoon Siddiqui <shamoon@develop.io>
2020-01-20 11:10:16 -0500

    Back to 1250 iterations

--- evaluate.sh
+++ evaluate.sh
@@ -6,6 +6,6 @@ for snr in 0 5 10 15 20 25; do
     pipenv run python evaluate.py \
         --noise "$1" \
         --snr "$snr" \
-        --iterations 2 \
+        --iterations 1250 \
         ${2:+--enhancement "$2"}
 done
